acf325cd2998579445670b7edb1d001f841a0b9d
= <=>
2021-03-07 16:26:01 +0530

    Modify 123

--- TestCases/test_Place_Order_New_User.py
+++ TestCases/test_Place_Order_New_User.py
@@ -38,7 +38,7 @@ class Test_Place_Order_New_User:
         self.cart_summary.click_continue_checkout_button()
 
         self.chkout_auth = Checkout_Auth_Page(self.driver)
-        self.chkout_auth.enter_email("mytestqa009@gmail.com")
+        self.chkout_auth.enter_email("mytestdev1@gmail.com")
 
         self.chkout_register = Checkout_RegisterUser_Page(self.driver)
         self.chkout_register.test_checkout_register_user("Ram","Chigari","ITC","Vijaynagar","Bangalore")
